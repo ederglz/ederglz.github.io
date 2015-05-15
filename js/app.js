44d3fa1248491a67851e349fb55d741266443e8b
Ederyairr <ederyairnglz@gmail.com>
2015-05-15 03:55:32 -0500

    adds functionality to the form

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 $(function(){
 	$('#btn').on('click', submitData);
 
-	function submitData() {
-
+	function submitData(e) {
+		e.preventDefault();
+		$('#Loading').addClass('u-display')
 		var name = $('#Name').val();
 		var mail = $('#Mail').val();
 		var subject = $('#Subject').val();
@@ -13,7 +14,9 @@ $(function(){
 			subject: subject,
 			content: content
 		}
-		$.post("https://openws.herokuapp.com/contact?apiKey=dfb51db4f812f136ae94541542a6e8ef", contactData)
+		$.post("https://openws.herokuapp.com/contact?apiKey=dfb51db4f812f136ae94541542a6e8ef", contactData).done(function(){
+			$('#Loading').removeClass('u-display')
+		})
 	}
 
 
